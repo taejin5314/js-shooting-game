c08668eb805dec4dc662ae9842fa724b083be358
taejin <taejin5314@gmail.com>
2021-05-05 22:18:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ class Projectile {
     ctx.save();
     ctx.translate(canvas.width / 2, canvas.height / 2);
     ctx.rotate(this.angle);
-    ctx.drawImage(projectileImage, 0, 32, 32, 32, this.x, this.y, this.size, this.size)
+    ctx.drawImage(projectileImage, 0, 32, 32, 32, this.x - this.size, this.y - this.size, this.size, this.size)
     ctx.restore();
     // ctx.fillStyle = 'red';
     // ctx.fillRect(this.posX - 15, this.posY - 15, this.size, this.size)
